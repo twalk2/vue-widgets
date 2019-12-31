467f032f45105fc6958aa8b49d6f42f14a1a62fc
twalk2 <2twalk2@gmail.com>
2019-12-31 13:10:51 -0800

    Added styling with tailwind and started toggle view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="h-screen flex flex-col items-center justify-center">
+    <div class="text-6xl m-10">Vue Widgets</div>
+    <div class="text-3xl m-10">Check them out!</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
---
+++ src/views/Toggle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Hello</div>
+    <button @click="toggle()">Toggle</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: true
+    };
+  },
+  methods: {
+    toggle: function() {
+      this.greeting = !this.greeting;
+    }
+  }
+};
+</script>
+<style></style>;
