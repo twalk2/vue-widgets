1e407b49b73c75e0a44471b4010753f6df456017
twalk2 <2twalk2@gmail.com>
2020-01-01 12:34:49 -0800

    added ternary for the toggle button

--- src/views/Toggle.vue
+++ src/views/Toggle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex flex-col items-center justify-center">
-    <div class="text-6xl m-20" v-if="greeting">Hello</div>
-    <div class="text-6xl m-20" v-else>Goodbye</div>
+    <div class="text-6xl m-20">{{ greeting ? "Hello" : "Goodbye" }}</div>
+    <!-- <div class="text-6xl m-20" v-else>Goodbye</div> -->
     <button
       class="m-20 text-4xl bg-red-500 p-1 border-2 border-black rounded-lg outline-none"
       @click="toggle()"
