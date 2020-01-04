7839aba40c51c7253dc5a066300456807b7edac0
twalk2 <2twalk2@gmail.com>
2020-01-04 10:19:21 -0800

    added more styles and font size to FontSizer

--- src/views/FontSizer.vue
+++ src/views/FontSizer.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    <div>px</div>
-    <div>
-      <button>Shrink</button>
-      <button>Grow</button>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <div class="m-20" v-bind:style="{ 'font-size': `${size}px` }">
+      {{ size }}px
+    </div>
+    <div class="flex m-20">
+      <button
+        class="m-20 text-4xl bg-red-500 p-1 border-2 border-black rounded-lg outline-none"
+        @click="
+          {
+            {
+              `$size` + 10;
+            }
+          }
+        "
+      >
+        Shrink
+      </button>
+      <button
+        class="m-20 text-4xl bg-red-500 p-1 border-2 border-black rounded-lg outline-none"
+      >
+        Grow
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      size: 20
+    };
+  }
+};
 </script>
