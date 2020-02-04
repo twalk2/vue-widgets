236b524c5a663d53fe577435058f62c81a7348a5
twalk2 <2twalk2@gmail.com>
2020-02-03 19:30:13 -0700

    Installed moment and added current date and time to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,18 @@
   <div class="h-screen flex flex-col items-center justify-center">
     <div class="text-6xl m-10">Vue Widgets</div>
     <div class="text-3xl m-10">Check them out!</div>
+    <div class="text-2xl">{{ getHumanDate() }}</div>
   </div>
 </template>
 
 <script>
-export default {};
+import moment from "moment";
+
+export default {
+  methods: {
+    getHumanDate: function() {
+      return moment().format("MMMM Do YYYY, h:mm a");
+    }
+  }
+};
 </script>
